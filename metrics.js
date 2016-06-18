879c813466d5c89085d545ca197b9da8f2c52362
Buzut <blog@buzut.fr>
2016-06-18 09:14:47 +0200

    added files

---
+++ metrics.js
@@ -0,0 +1,57 @@
+#!/usr/bin/nodejs
+
+const hostname = 'myHost';
+
+// currently https://github.com/Buzut/systeminformation needed
+const si = require('systeminformation');
+const _ = require('lodash');
+const ps = require('current-processes');
+
+// don't start it at the same time as ps as it generates load by itself
+setTimeout(function(){
+    si.getAllData((data) => {
+        console.log(`cpu_load,host=${hostname} value=${data.currentLoad.currentload}`);
+
+        // http://www.binarytides.com/linux-command-check-memory-usage/
+        // Used - (buffered + cached) = buffer/cache used
+        // Total - buffer/cache used = really avail
+
+        var ram = data.mem.used * (100 / data.mem.total);
+        var swap = (data.mem.swaptotal === 0) ? 0 : data.mem.swapused * (100 / data.mem.swaptotal);
+        console.log(`ram_used,host=${hostname} value=${ram}`);
+        console.log(`swap_used,host=${hostname} value=${swap}`);
+
+        // volumes IOPS
+        console.log(`disk_rIOPS,host=${hostname} value=${data.fsStats.rIOPS}`);
+        console.log(`disk_wIOPS,host=${hostname} value=${data.fsStats.wIOPS}`);
+        console.log(`disk_tIOPS,host=${hostname} value=${data.fsStats.totalIOPS}`);
+
+        // network transmit rates
+        var netRx = data.networkStats.rx_sec;
+        var netTx = data.networkStats.tx_sec;
+        console.log(`network_rx,host=${hostname} value=${netRx}`);
+        console.log(`network_tx,host=${hostname} value=${netTx}`);
+
+        // fs usage
+        data.fsSize.forEach((el) => {
+            var watchedMounts = ['/', '/dev', '/home'];
+
+            if (watchedMounts.indexOf(el.mount) !== -1) {
+                console.log(`fs_usage,host=${hostname},volume=${el.mount} value=${el.use}`);
+            }
+        });
+    });
+}, 500);
+
+ps.get((err, processes) => {
+    // get five most CPU hungry
+    var sorted = _.sortBy(processes, 'cpu');
+    var top5  = sorted.reverse().splice(0, 6);
+
+    top5.forEach((el) => {
+        // avoid the observer effect
+        if (el.name === 'metrics.js') return;
+        console.log(`process_cpu,host=${hostname},name=${el.name} value=${el.cpu}`);
+        console.log(`process_mem,host=${hostname},name=${el.name} value=${el.mem.usage}`);
+    });
+});
