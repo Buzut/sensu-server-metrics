5db39f21a8eb93a6c6d24982582d82eed152e6a9
Buzut <blog@buzut.fr>
2017-07-01 09:11:13 +0200

    new version ES6 + last systeminformation version

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Sensu metric check is a metrics check written in nodejs that gathers server perf
 
 ## Prerequisites
 
-* As this check is written in NodeJS, [Node](https://nodejs.org/en/) has to be installed on the system running it.
+* As this check is written in NodeJS, [Node 6+](https://nodejs.org/en/) has to be installed on the system running it.
 
 * The check was initialy written to store data in InfluxDB. Therefore it outputs metrics on `stdin`
 and needs an appropriate handler. I have written one in Node that is [available as a Gist](https://gist.github.com/Buzut/dd49ee9c9b589f1035296ef96e63698e).
@@ -13,6 +13,26 @@ and needs an appropriate handler. I have written one in Node that is [available 
 In your Sensu plugins directory run:
 
 `npm install sensu-server-metrics`
+
+Add `defaultIface` if you want to get stats about your network rx & tx and `services` (as a list of comma seperated system service names) to get their CPU & RAM usage.
+Exemple:
+
+```json
+{
+  "client": {
+    "name": "lb1",
+    "system": "Ubuntu 16.04",
+    "cpu": "Intel(R) Atom(TM) CPU  C2750  @ 2.40GHz",
+    "cpu_cores": "8",
+    "cpu_threads_per_core": "1",
+    "ram": "16010MB",
+    "defaultIface": "enp0s20f0",
+    "address": "63.17.218.135",
+    "subscriptions": ["default", "httpd80"],
+    "services": "nginx,fail2ban,exim4"
+  }
+}
+```
 
 Finally add the check to your checks file:
 
@@ -33,7 +53,7 @@ Finally add the check to your checks file:
 ## Metrics
 
 The check gathers the following metrics:
-* 5 most cpu hungry processes cpu and memory usage
+* given system services CPU and RAM usage (in %)
 * cpu load (in %)
 * RAM usage (in %)
 * SWAP usage (in %)
--- serverMetrics.js
+++ serverMetrics.js
@@ -1,79 +1,72 @@
 #!/usr/bin/nodejs
 
-// currently https://github.com/Buzut/systeminformation needed
 const fs = require('fs');
 const si = require('systeminformation');
-const _ = require('lodash');
-const ps = require('current-processes');
 
 // read config file
-var clientJSON;
+let clientJSON;
 try {
     clientJSON = '/etc/sensu/client.json';
     fs.statSync(clientJSON);
 }
+
 catch (e) {
     try {
         clientJSON = '/etc/sensu/conf.d/client.json';
         fs.statSync(clientJSON);
     }
+
     catch (e) {
         console.log('client.json can\'t be found or read neither in sensu root nor in conf.d');
         process.exit(3);
     }
 }
 
-var clientConf = fs.readFileSync(clientJSON);
+let clientConf = fs.readFileSync(clientJSON);
 clientConf = JSON.parse(clientConf);
+
 const hostname = clientConf.client.name;
+const defaultIface = clientConf.client.defaultIface;
+const services = clientConf.client.services;
 
-// don't start it at the same time as ps as it generates load by itself
-setTimeout(function () {
-    si.getAllData((data) => {
-        console.log(`cpu_load,host=${hostname} value=${data.currentLoad.currentload}`);
+si.getDynamicData(services, defaultIface, (data) => {
+    console.log(`cpu_load,host=${hostname} value=${data.currentLoad.currentload}`);
 
-        // http://www.binarytides.com/linux-command-check-memory-usage/
-        // Used - (buffered + cached) = buffer/cache used
-        // Total - buffer/cache used = really avail
+    // http://www.binarytides.com/linux-command-check-memory-usage/
+    // Used - (buffered + cached) = buffer/cache used
+    // Total - buffer/cache used = really avail
 
-        var ram = (data.mem.used - data.mem.buffcache) * (100 / data.mem.total);
-        var swap = (data.mem.swaptotal === 0) ? 0 : data.mem.swapused * (100 / data.mem.swaptotal);
-        console.log(`ram_used,host=${hostname} value=${ram}`);
-        console.log(`swap_used,host=${hostname} value=${swap}`);
+    var ram = (data.mem.used - data.mem.buffcache) * (100 / data.mem.total);
+    var swap = (data.mem.swaptotal === 0) ? 0 : data.mem.swapused * (100 / data.mem.swaptotal);
+    console.log(`ram_used,host=${hostname} value=${ram}`);
+    console.log(`swap_used,host=${hostname} value=${swap}`);
 
-        // volumes IOPS
-        console.log(`disk_rIOPS,host=${hostname} value=${data.fsStats.rIOPS}`);
-        console.log(`disk_wIOPS,host=${hostname} value=${data.fsStats.wIOPS}`);
-        console.log(`disk_tIOPS,host=${hostname} value=${data.fsStats.totalIOPS}`);
-
-        // network transmit rates
-        var netRx = data.networkStats.rx_sec;
-        var netTx = data.networkStats.tx_sec;
-        console.log(`network_rx,host=${hostname} value=${netRx}`);
-        console.log(`network_tx,host=${hostname} value=${netTx}`);
-
-        // fs usage
-        data.fsSize.forEach((el) => {
-            var watchedMounts = ['/', '/dev', '/home'];
-
-            if (watchedMounts.indexOf(el.mount) !== -1) {
-                console.log(`fs_usage,host=${hostname},volume=${el.mount} value=${el.use}`);
-            }
-        });
+    data.services.forEach((service) => {
+        if (service.running) {
+            console.log(`process_cpu,host=${hostname},name=${service.name} value=${service.pcpu}`);
+            console.log(`process_mem,host=${hostname},name=${service.name} value=${service.pmem}`);
+        }
     });
-}, 1000);
 
-ps.get((err, processes) => {
-    // get five most CPU hungry
-    var sorted = _.sortBy(processes, 'cpu');
-    var top5  = sorted.reverse().splice(0, 6);
+    // fs usage
+    data.fsSize.forEach((el) => {
+        const watchedMounts = ['/', '/dev', '/home'];
 
-    top5.forEach((el) => {
-        // avoid the observer effect
-        if (el.name === 'serverMetrics.js') return;
+        if (watchedMounts.indexOf(el.mount) !== -1) {
+            console.log(`fs_usage,host=${hostname},volume=${el.mount} value=${el.use}`);
+        }
+    });
 
-        var cleanName = el.name.replace(' ', '_');
-        console.log(`process_cpu,host=${hostname},name=${cleanName} value=${el.cpu}`);
-        console.log(`process_mem,host=${hostname},name=${cleanName} value=${el.mem.usage}`);
+    si.networkStats(defaultIface, (netData) => {
+        // network transmit rates
+        console.log(`network_rx,host=${hostname} value=${netData.rx_sec}`);
+        console.log(`network_tx,host=${hostname} value=${netData.tx_sec}`);
+    });
+
+    si.disksIO((diskDatas) => {
+        // volumes IOPS
+        console.log(`disk_rIOPS,host=${hostname} value=${diskDatas.rIO_sec}`);
+        console.log(`disk_wIOPS,host=${hostname} value=${diskDatas.wIO_sec}`);
+        console.log(`disk_tIOPS,host=${hostname} value=${diskDatas.tIO_sec}`);
     });
 });
